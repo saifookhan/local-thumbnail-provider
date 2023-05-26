2ed79b5938554e83834bb634919fa73b92273f2c
saifookhan <isaifookhan@gmail.com>
2023-05-26 15:48:40 +0200

    update tsconfig => module: commonjs, target es15

--- lib/src/LocalThumbnailProvider.d.ts
+++ lib/src/LocalThumbnailProvider.d.ts
@@ -4,7 +4,7 @@ import { formats } from "./ThumbnailProvider";
 export default class LocalThumbnailProvider implements ThumbnailProvider {
     storage: Storage;
     constructor(storage: Storage);
-    getThumbUrl(id: string, format: keyof typeof formats): Promise<string | null>;
+    getThumbUrl(id: string, format: keyof typeof formats): Promise<string>;
     createThumb(id: string, thumbId: string, format: keyof typeof formats): Promise<void>;
     resize(src: string, dest: string, size: ThumbnailSize): Promise<sharp.OutputInfo>;
 }
